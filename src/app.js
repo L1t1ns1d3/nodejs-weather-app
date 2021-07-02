5377865b5ae62961b9eca7aa6135e0434fd57eb3
Ahmed M. Elhady <a.m.elhady@outlook.com>
2021-07-02 16:42:52 +0200

    Init commit

---
+++ src/app.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const forecast = require('./utils/forecast');
+const geocode = require('./utils/geocode');
+
+// init the app
+const app = express();
+
+// Define paths for Express configs
+const PUBLIC_DIR = path.join(__dirname, '../static');
+const VIEWS_DIR = path.join(__dirname, '../templates/views');
+const PARTIALS_DIR = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', VIEWS_DIR);
+hbs.registerPartials(PARTIALS_DIR);
+
+// Setup static directory to serve.
+app.use(express.static(PUBLIC_DIR));
+
+// routes
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Ahmed Elhady',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About me',
+    name: 'Ahmed Elhady',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help page',
+    name: 'Ahmed Elhady',
+    msg: 'If you want help, please contact me at:',
+    email: 'a.m.elhady@outlook.com',
+  });
+});
+
+app.get('/weather', (req, res) => {
+  if (!req.query.address)
+    return res.send({ error: 'You must provide an address query.' });
+  const address = req.query.address;
+  geocode(address, (error, { longitude, latitude, location } = {}) => {
+    if (error) return res.send({ error });
+    forecast(longitude, latitude, (error, forecastData) => {
+      if (error) return res.send({ error });
+      res.send({
+        forecast: forecastData,
+        location,
+        address,
+      });
+    });
+  });
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    errorMsg: 'Help article is not found.',
+    name: 'Ahmed Elhady',
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    errorMsg: 'This page is not found.',
+    name: 'Ahmed Elhady',
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000.');
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+const forecast = (longitude, latitude, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=0ed99994cbc7d3f0d5e78cad376ff24f&query=${latitude},${longitude}&units=f`;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error)
+      callback('Unable to connect to weatherstack service!', undefined);
+    else if (body.error)
+      callback(
+        'Please specify a valid location identifier using the query parameter.',
+        undefined
+      );
+    else
+      callback(
+        undefined,
+        `It is currently ${body.current.temperature}. It feels like ${body.current.feelslike}.`
+      );
+  });
+};
+
+module.exports = forecast;
